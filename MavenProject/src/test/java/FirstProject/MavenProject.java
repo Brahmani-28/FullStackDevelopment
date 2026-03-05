b60195324ed09bf79e28e7e031e663ae6526050b
Brahmani-28 <2420030150@klh.edu.in>
2026-03-05 12:33:19 +0530

    Add files via upload

---
+++ MavenProject/src/test/java/FirstProject/MavenProject.java
@@ -0,0 +1,10 @@
+package FirstProject;
+
+public class MavenProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi this is muy new Maven project");
+	}
+
+}
