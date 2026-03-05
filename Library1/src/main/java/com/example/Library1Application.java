b51b219800eaddeaa36c591298a1d969c358ad54
Brahmani-28 <2420030150@klh.edu.in>
2026-03-05 12:20:10 +0530

    Add files via upload

---
+++ Library1/src/main/java/com/example/Library1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication
+public class Library1Application {
+
+ public static void main(String[] args) {
+  SpringApplication.run(Library1Application.class, args);
+ }
+
+}
